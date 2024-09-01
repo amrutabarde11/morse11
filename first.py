d8cd22aa002f5f1bdab762bf8f02ff43aadc8f51
amrutabarde11 <amrutabarde15@gmail.com>
2024-09-01 16:16:07 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,119 @@
+# print("hello world")
+# a=20
+# b=30
+# c=(a*b)
+
+# print (c)
+# # ctrl+/ for comment
+# name="Aaditya " #starting
+# age=18 #intiger data type
+# salery= 1500000 # float 
+# a=True #bollean intiger value
+
+# comparision operatiors
+# print (a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+#logical oprators
+# a = True
+# b = False
+# print(a and b)
+# print(a or b)
+# print(not b)
+
+#arithmatic opp
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+#assignment opp..
+# a=10
+# b=20
+# a+=3
+# b-=3
+# print(a)
+# print(b)
+# a/=9
+# b*=80
+# print(a)
+# print(b)
+
+# input function
+# a= input ("enter your name")
+# b= int(input("enter your no-"))
+# print(a)
+# print (b)
+
+# task
+# a=int(input("num 1    "))
+# b=int(input("num 2    "))
+# sum=a+b
+# print(sum)
+# print("task complete tannu")
+
+# control flow 
+# if statement
+# x=151
+# if x > 5:
+#    print ("x is greater than 5 ")
+
+#    if x != 10:
+#       print ("x is equal to 10")
+#       #alt+shift+down same line repeter
+
+# temperature=10
+# if temperature>35:
+#     print("its very hot")
+# elif temperature > 25:
+#     print("its warm")
+# elif temperature >15:
+#     print("its cold")
+# else:
+#     print("jam gaya")
+
+# for loop
+# to print the no one to 1000
+# for i in range(1, 1001):
+#     print (i)
+
+#while loops--
+# i=1
+# while(i<11):
+#     print(i)
+#     i+=1
+
+# my_list = ["ig-sanskari_aaditya9", 9.8,9.7]
+# print(my_list)
+
+#print(my_list[4]) #accessing list
+# my_list[2]="aaditya" #modifing list
+# print(my_list)
+# my_list.append("singh") #add data type
+# print(my_list)
+# my_list.remove("apple")
+# print(my_list)
+# print(my_list[0])
+# my_list[0]="aaditya"
+# my_list.remove(9.8)
+# print(my_list)
+
+
+# 11.reverce
+# 11.clear()
+# print(11)
+
+# tuple
+# creating tuples
+
+my_tuple =(10, "apple", 123)
+print(my_tuple)
+
+# accessing tuple
+first_item=my_tuple[0]
+print(first_item)
+
